0a27bf774ecb51237da9e1767545e19892863cc8
Anton Roslund <enari@Antons-MacBook-Air.local>
2017-04-13 15:53:13 +0200

    Initial Commit

---
+++ DVA338/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DVA338
+//
+//  Created by Anton Roslund on 2017-04-13.
+//  Copyright © 2017 Anton Roslund. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
